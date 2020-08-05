c73af182c013575e864fcd357084cef269eb57c9
ansari <sairaansari12@gmail.com>
2020-08-05 16:03:46 +0530

    Payment Module

--- controllers/dashboard/dpayment.controller.js
+++ controllers/dashboard/dpayment.controller.js
@@ -120,6 +120,91 @@ if(fromDate!="" && toDate!="")
 
 });
 
+
+app.post('/search',adminAuth, async (req, res, next) => {
+  
+try {
+  var params=req.body
+  var transactionStatus =  ['0','1','2']
+  var fromDate =  ""
+  var toDate =  ""
+
+
+  var page =1
+  var limit =50
+  if(params.page) page=params.page
+  if(params.limit) limit=parseInt(params.limit)
+  var offset=(page-1)*limit
+
+
+
+  
+      const findData = await PAYMENT.findAndCountAll({
+        order: [
+          ['createdAt', 'DESC'],  
+      ],
+      where :where,
+      
+      include: [
+        {model: USER , attributes: ['id','firstName','lastName',"phoneNumber","countryCode","email","image"],
+        required: true, 
+        where: {
+          [Op.or]: [
+            { 
+              firstName: {
+                [Op.like]: `%${params.search}%`
+              }
+            }
+          ]
+        }},       
+        {model: ORDERS , attributes: ['id','createdAt','orderNo'],required:true},       
+
+      
+      ],
+      distinct:true,
+      offset: offset, limit: limit ,
+
+    });
+
+    console.log(findData)
+
+    var countDataq = await PAYMENT.findAll({
+      attributes: ['transactionStatus','paymentState',
+        [sequelize.fn('sum', sequelize.col('amount')), 'totalSum'],
+        [sequelize.fn('COUNT', sequelize.col('transactionStatus')), 'count'],
+       
+
+      ],
+      group: ['transactionStatus'],
+    where :where1});
+
+
+    var escrowDataq = await PAYMENT.findAll({
+      attributes: ['paymentState',
+        [sequelize.fn('sum', sequelize.col('amount')), 'totalSum'],
+        [sequelize.fn('COUNT', sequelize.col('paymentState')), 'count'],
+       
+
+      ],
+      group: ['paymentState'],
+    where :where1});
+
+    var userDtaa={}
+    userDtaa.data=findData
+    userDtaa.counts=countDataq
+    userDtaa.escrow=escrowDataq
+
+
+    return responseHelper.post(res, appstrings.success, userDtaa);
+
+  } catch (e) {
+    console.log(e)
+    return responseHelper.error(res, e.message, 400);
+  }
+
+
+});
+
 app.post('/status',adminAuth,async(req,res,next) => { 
     
     var params=req.body
