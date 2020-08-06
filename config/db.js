34a3016e1bc58852c1dc31dc0f8aaf2c5513b5d2
Sandeep Singh <singhsandeep@seasiainfotech.com>
2020-08-06 12:01:43 +0530

    employee and schedule module

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: 'root',
-    password: '',
+    password: 'mind@123',
     database: 'fooddeliveryservicesNew',
     host: 'localhost',
     dialect: 'mysql',
